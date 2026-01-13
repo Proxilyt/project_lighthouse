e3584556f204fa984d019c3f9128487c9d133fdb
hga50 <himank_goel@sfu.ca>
2026-01-13 15:45:42 -0800

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
-import { APP_NAME } from '../constants'
+import { APP_NAME } from "../constants";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        <div className="logo">{APP_NAME}</div>
-        <button className="get-started-btn">Get Started</button>
+        <div className="header__left">
+          <div className="logo">
+            <span className="logo-icon"> <img src="/src/assets/logo_light_mode.svg" alt="Proxilyt Logo" /></span>
+            <span className="logo-text">Proxilyt</span>
+          </div>
+        </div>
+        <nav className="header__right">
+          <a href="#features" className="nav-link">
+            Features
+          </a>
+          <button href="#cta-section" className="cta-button">Join Waitlist</button>
+        </nav>
       </div>
     </header>
-  )
+  );
 }
