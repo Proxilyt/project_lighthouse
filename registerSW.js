3353ea9661084e72ec4dd9c2d99a16f0da9b30e1
hga50 <himank_goel@sfu.ca>
2026-01-15 10:04:46 -0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/project_lighthouse/sw.js', { scope: '/project_lighthouse/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
