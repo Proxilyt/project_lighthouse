c39c813333fc2efdd20e8532df6ba82833c3ca44
hga50 <himank_goel@sfu.ca>
2026-01-14 22:22:49 -0800

    added logo component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { APP_NAME } from "../constants";
+import logo from "../assets/logo_light_mode.svg";
 
 export default function Header() {
   return (
@@ -6,15 +7,19 @@ export default function Header() {
       <div className="header-container">
         <div className="header__left">
           <div className="logo">
-            <span className="logo-icon"> <img src="src/assets/logo_light_mode.svg" alt="Proxilyt Logo" /></span>
-            <span className="logo-text">Proxilyt</span>
+            <span className="logo-icon">
+              <img src={logo} alt="Proxilyt Logo" />
+            </span>
+            <span className="logo-text">{APP_NAME}</span>
           </div>
         </div>
         <nav className="header__right">
           <a href="#features" className="nav-link">
             Features
           </a>
-          <button className="cta-button"><a href="#cta-section">Join Waitlist</a></button>
+          <button className="cta-button">
+            <a href="#cta-section">Join Waitlist</a>
+          </button>
         </nav>
       </div>
     </header>
